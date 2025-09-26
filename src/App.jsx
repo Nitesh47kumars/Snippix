41b23e4a45370a4a7d606ed10ad2a6330b6203b9
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-26 19:20:02 +0530

    Start Building SnippitPreview Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
+import SnippetPreview from './Components/SnippetPreview'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <h1 className='text-primary font-bold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SnippetPreview/>
     </>
   )
 }
---
+++ src/Components/SnippetPreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SnippetPreview = () => {
+  return (
+    <div className='h-[80vh] bg-black'>
+      
+    </div>
+  )
+}
+
+export default SnippetPreview
