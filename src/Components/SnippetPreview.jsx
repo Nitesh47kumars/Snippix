bb76d4917a7f6053e00a962a6f133daad56cecf9
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-27 20:55:03 +0530

    Change Styling of Export Button

--- src/Components/SnippetPreview.jsx
+++ src/Components/SnippetPreview.jsx
@@ -3,7 +3,7 @@ import SnippetCode from './SnippetCode';
 import GradientWrappper from './GradientWrapper'
 const SnippetPreview = () => {
   return (
-    <div className="h-[80vh] m-5">
+    <div className="h-[75vh] m-5">
       <div className="border border-white/30 rounded-2xl h-full w-full flex items-center justify-center">
         <GradientWrappper>
             <SnippetCode />
