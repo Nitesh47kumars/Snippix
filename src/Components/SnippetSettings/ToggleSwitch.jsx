1ffc644f34da99a28dcb8dd9610a4ec77371c1f4
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-27 21:15:23 +0530

    Add Styled Component to ToggleSwitch

--- src/Components/SnippetSettings/ToggleSwitch.jsx
+++ src/Components/SnippetSettings/ToggleSwitch.jsx
@@ -1,21 +1,9 @@
+import Switch from '../../utiles/Switch'
 export default function ToggleSwitch({ label, checked }) {
   return (
     <div>
       <label className="block mb-2 text-xs font-medium text-neutral-400">{label}</label>
-      <button
-        type="button"
-        role="switch"
-        aria-checked={checked}
-        className={`inline-flex h-[1.15rem] w-8 items-center rounded-full transition-all ${
-          checked ? 'bg-primary' : 'bg-input dark:bg-input/80'
-        }`}
-      >
-        <span
-          className={`block size-4 rounded-full transition-transform ${
-            checked ? 'translate-x-[calc(100%-2px)] bg-primary-foreground' : 'translate-x-0 bg-foreground'
-          }`}
-        />
-      </button>
+      <Switch checked={checked}/>
     </div>
   );
 }
