d3a59c49c2289b87a52295bbd4930b51f59aaa66
Nitesh47kumars <niteshshah028@gmail.com>
2025-09-30 22:08:13 +0530

    Fix Height Issue & recover SnippetSettings

--- src/Components/SnippetPreview.jsx
+++ src/Components/SnippetPreview.jsx
@@ -3,8 +3,8 @@ import SnippetCode from './SnippetCode';
 import GradientWrappper from './GradientWrapper'
 const SnippetPreview = () => {
   return (
-    <div className="h-[75vh] m-5">
-      <div className="border border-white/30 rounded-2xl h-full w-full flex items-center justify-center">
+    <div className="min-h-[75vh] my-5 mx-4 border border-white/30 rounded-2xl flex items-center justify-center">
+      <div className="h-full w-full flex items-center justify-center">
         <GradientWrappper>
             <SnippetCode />
         </GradientWrappper>
--- src/Components/SnippetSettings/SnippetSettings.jsx
+++ src/Components/SnippetSettings/SnippetSettings.jsx
@@ -9,7 +9,7 @@ export default function SnippetSettings() {
   const [padding, setPadding] = useState(64);
   return (
     <div className='flex justify-center h-full pb-6'>
-        <div className="rounded-xl border shadow-sm p-6 bg-neutral-900/90">
+        <div className="rounded-xl border border-neutral-700 shadow-sm p-6 bg-neutral-900/90">
         <div className="flex flex-wrap gap-4 sm:gap-6">
             <SnippetSelect label="Theme" value="sublime" iconColor="from-rose-400 via-fuchsia-500 to-indigo-500" />
             <SnippetSelect label="Language" value="JavaScript/JSX" />
