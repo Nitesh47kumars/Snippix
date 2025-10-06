9aadfb91f4d733266a4d4cd1d4421dbe04d1e1b9
Nitesh47kumars <niteshshah028@gmail.com>
2025-10-06 22:38:29 +0530

    Fix Gradient Background Padding

--- src/Components/SnippetPreview.jsx
+++ src/Components/SnippetPreview.jsx
@@ -5,7 +5,7 @@ import GradientWrapper from './GradientWrapper';
 const SnippetPreview = () => {
   return (
     <div className="h-[78vh] m-4">
-      <div className="border border-neutral-800 rounded-2xl h-full w-full flex items-center justify-center">
+      <div className="border border-neutral-800 rounded-2xl h-full w-full flex items-center justify-center overflow-hidden">
         <GradientWrapper>
             <SnippetCode />
         </GradientWrapper>
