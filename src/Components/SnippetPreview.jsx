195068f42952bb0e0f914e14bafe8d0afd4ac5fb
Nitesh47kumars <niteshshah028@gmail.com>
2025-10-16 21:32:05 +0530

    Update SnippetPreview Component(Fix Height Issue

--- src/Components/SnippetPreview.jsx
+++ src/Components/SnippetPreview.jsx
@@ -3,15 +3,14 @@ import SnippetCode from './SnippetCode';
 import GradientWrapper from './GradientWrapper';
 import { MyContext } from '../MyContext';
 
-
 const SnippetPreview = () => {
-  const {exportRef} = useContext(MyContext);
-  
+  const { exportRef } = useContext(MyContext);
+
   return (
-    <div className="max-sm:h-[60vh] h-[75vh] m-4 sm:mb-6">
-      <div className="border border-neutral-800 rounded-2xl h-full w-full flex items-center justify-center overflow-hidden">
-        <GradientWrapper ref={exportRef} >
-            <SnippetCode />
+    <div className="m-4 sm:mb-6 max-sm:min-h-[60vh] min-h-[75vh]">
+      <div className="border border-neutral-800 rounded-2xl py-17 max-sm:py-7 w-full h-full flex items-center justify-center overflow-hidden">
+        <GradientWrapper ref={exportRef}>
+          <SnippetCode />
         </GradientWrapper>
       </div>
     </div>
