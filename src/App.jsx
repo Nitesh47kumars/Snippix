3777dfcc88d3c5f7e38c9aced0bfd8104338fd5a
Nitesh47kumars <niteshshah028@gmail.com>
2026-01-09 22:17:24 +0530

    Add fixing Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { MyProvider } from './MyContext'
 const App = () => {
   return (
     <>
+      <div className="w-full sticky top-0 z-190 bg-yellow-500/10 border-b border-yellow-500/30 text-yellow-300 text-sm text-center py-2 px-4">
+        This site is currently under maintenance. You may experience temporary issues.
+      </div>
       <MyProvider>
         <SnippetPreview/>
         <SnippetSettings/>
